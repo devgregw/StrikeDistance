9ed03e361d5b813dfb2383a862b09dd209b21b5e
devgregw <devgregw@outlook.com>
2015-03-19 16:04:10 -0500

    Worked on Android projects

---
+++ Android/StrikeDistance-AndroidPhoneAndTablet/gen/com/devgregw/strikedistance_androidphoneandtablet/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.devgregw.strikedistance_androidphoneandtablet;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
